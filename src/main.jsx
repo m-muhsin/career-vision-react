859cff585ce26867c333444c01294fc2e455515c
Muhammad Muhsin <mmuhsin@awesomemotive.com>
2025-03-12 17:15:26 +0530

    add analytics

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { registerCoreBlocks } from '@wordpress/block-library'
+import { Analytics } from "@vercel/analytics/react"
 
 import App from './App.jsx'
 import './index.css'
@@ -10,5 +11,6 @@ registerCoreBlocks();
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
+    <Analytics />
   </StrictMode>,
 )
